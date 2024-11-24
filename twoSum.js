03677e6e60579548749ef3edba88562aa3058677
Callum Chalifour <callumblair42@gmail.com>
2024-11-24 13:14:39 -0800

    twoSum Complete

---
+++ twoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map(); // Store visited numbers and their indices
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return null; // Return null if no solution is found
+};
